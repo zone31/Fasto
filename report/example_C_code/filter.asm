dfb80709ec839d59875268462492e95dec238c92
fluttershy <fluttershy@fluttershy-530U3C-530U4C.(none)>
2014-12-15 17:37:52 +0100

    Made example C code for scan.

--- report/example_C_code/filter.asm
+++ report/example_C_code/filter.asm
@@ -1,17 +1,48 @@
-# Assuming the pointer to the start array is saved in $t1
+# I assume a pointer to x is saved in the register $a0 and that the first
+# word in that array is the size of the array.
 
-lw  $t2, 0($t1)              # Load size of array to $t2
+# Initialization
+addi $s0, $zero, 0           # i = 0
+lw   $s1, 0($a0)             # $s1 equals size of array
+addi $s2, $s1, 4             # $s2 points to the first element in the array x
 
-li  $s1, 0                   # Load 0 to $s1
-add $s1, $s1, $hp            # Save the heap pointer to $s1
-li  $t3, 4                   # Load 4 to $t3
-mul $t3, $t3, $t2            # The size of the array in bytes
-add $hp, $hp, $t3            # The heap pointer is incremented appropriately
+# Space allocation
+add  $a0 $zero, $s1          # $a0 contain the size of the array
+addi $t0, $zero, 4           # Load 4 to $t0
+mul  $a0, $a0, $t0           # $a0 contains the number of bytes, not the size
+addi $v0, 9                  # syscall 9, heap allocation
+syscall                      # Address to allocated space is in $v0
+add  $s3, $v0, $zero         # Pointer to allocated space is saved in $s3
 
-li  $t3, 1                   # $t3 points out the next element in the array
-While:
-    beq $t2, $zero, EndWhile # When $t2 is zero we jump out of the while loop
+# Init while
+addi $s4, $s3, 4             # $s4 point to first element in y
 
-    $
+# While loop
+WhileCond:
+    slt $t0, $s0, $s1        # $t0 is 1 if i < size, 0 otherwise
+    bne $t0, $zero, EndWhile # Branch if i >= size
+
+WhileCode:
+    lw  $a0, 0($s2)          # The argument to a function is set to M[$s2]
+    jal f                    # Call the function f
+    beq $v0, $zero, IfFalse  # Branch if return val from f is false
+
+    IfTrue:
+        lw   $t0, 0($s2)     # $s2 point to the i'th element
+        sw   $t0, 0($s4)     # $t0 is saved to $s4 (next element in y)
+        addi $s4, $s4, 4     # $s4 point to next element
+
+    IfFalse:
+    addi $s2, $s2, 4         # Increment $s2 to point to next element in x
+    addi $s0, $s0, 1         # i is incremented (for the while condition)
+    j   WhileCond            # Go back to the while condition
 
 EndWhile:
+    # Compute length of array y
+    sub  $t0, $s3, $s4       # $t0 is the length in bytes of array y
+    addi $t1, $t1, 4         # $t1 is 4
+    div  $t0, $t0, $t1       # $t0 is now the length of y
+    sw   $t0, 0($s3)         # Write array length of y to first word in y
+
+    # Add return values to return registers
+    addi $v0, $s3, 0         # $v0 point to the memoryblock with y
---
+++ report/example_C_code/scan.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int * scan(int *arr, int neutral, int arr_size, int (*func)(int, int));
+void print_int_arr(int *arr, int arr_size);
+int add(int a, int b);
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    int arr_size = 20;
+    int arr[arr_size];
+    int *arr_res;
+
+    for (i = 0; i < arr_size; i++)
+        arr[i] = i;
+
+    arr_res = scan(arr, 0, arr_size, add);
+
+    print_int_arr(arr_res, arr_size);
+}
+
+int * scan(int *arr, int neutral, int arr_size, int (*func)(int, int))
+{
+    int i, j, res;
+    int *arr_res = malloc(sizeof(int) * arr_size + 1); /* Plus 1 neutral el. */
+
+    arr_res[0] = neutral;
+
+    for (i = 0; i < arr_size; i++) {
+        res = func(arr_res[i], arr[i]);
+
+        arr_res[i+1] = res;
+    }
+
+    return arr_res;
+}
+
+void print_int_arr(int *arr, int size)
+{
+    int i;
+
+    for (i = 0; i < size-1; i++)
+        printf("%d, ", arr[i]);
+    printf("%d\n", arr[size-1]); /* Print last element with no comma. */
+}
+
+int add(int a, int b)
+{
+    return a + b;
+}
